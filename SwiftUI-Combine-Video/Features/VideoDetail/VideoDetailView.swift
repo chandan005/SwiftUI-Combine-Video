5f1964e8abd5a09b3f910f3a51126f845bcffcc6
chandansingh005 <chandan.singh@brauz.com>
2020-06-23 00:01:46 +0545

    download video with progress bar

--- SwiftUI-Combine-Video/Features/VideoDetail/VideoDetailView.swift
+++ SwiftUI-Combine-Video/Features/VideoDetail/VideoDetailView.swift
@@ -12,21 +12,23 @@ import AVKit
 
 struct VideoDetailView: View {
     
+    @Environment(\.presentationMode) var presentation
     @ObservedObject var viewModel: VideoDetailViewModel
     @State private var avController = AVPlayerViewController()
+    @State var showProgressBar = false
     
     var body: some View {
-        videoDetailView(self.viewModel.videoListItem)
+        videoView(self.viewModel.videoListItem)
         .onDisappear {
             self.avController.player?.pause()
         }
     }
     
-    private func videoDetailView(_ video: VideoListItem) -> some View {
+    private func videoView(_ video: VideoListItem) -> some View {
         ScrollView {
             VStack {
                 video.videoLink.map { url in
-                    VideoView(videoUrl: url , controller: avController)
+                    VideoView(videoUrl: self.viewModel.isInternetConnected ? url : FileManagerApi(fileName: "video.mp4").getFileUrl(), controller: avController)
                         .frame(height: UIScreen.main.bounds.height / 2.25)
                         .cornerRadius(5)
                 }
@@ -48,11 +50,34 @@ struct VideoDetailView: View {
                 }
             }.padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
             .navigationBarItems(trailing:
+                self.viewModel.isInternetConnected ?
+                Button(action: {
+                    self.showProgressBar.toggle()
+                    if !self.viewModel.didFinishDownloading {
+                        self.viewModel.startDownload()
+                    }
+                }) {
+                    Text("Download Video")
+                    Image(systemName: "square.and.arrow.down")
+                }.sheet(isPresented: self.$showProgressBar, onDismiss: {
+                    print(self.showProgressBar)
+                    self.viewModel.cancelDownload()
+                }) {
+                    CircularProgressView(currentProgress: self.$viewModel.currentProgress, percentage: self.$viewModel.percentage,  didFinishDownload: self.$viewModel.didFinishDownloading)
+                }
+                
+                :
                 Button(action: {
                 }) {
-                    Text("Download")
+                    Text("No Internet")
                     Image(systemName: "square.and.arrow.down")
-                }            )
+                }.sheet(isPresented: self.$showProgressBar, onDismiss: {
+                    print(self.showProgressBar)
+                    self.viewModel.cancelDownload()
+                }) {
+                    CircularProgressView(currentProgress: self.$viewModel.currentProgress, percentage: self.$viewModel.percentage,  didFinishDownload: self.$viewModel.didFinishDownloading)
+                }
+            )
         }
     }
 }
--- SwiftUI-Combine-Video/Features/VideoDetail/VideoDetailViewModel.swift
+++ SwiftUI-Combine-Video/Features/VideoDetail/VideoDetailViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 import Combine
 import Network
 
-final class VideoDetailViewModel:  ObservableObject {
+final class VideoDetailViewModel: NSObject, ObservableObject {
     
     @Published var videoListItem: VideoListItem
     @Published var currentProgress: String = ""
@@ -20,8 +20,87 @@ final class VideoDetailViewModel:  ObservableObject {
     @Published var isInternetConnected: Bool = true
     private var bag: Set<AnyCancellable> = []
     let download = Download()
+    let monitor = NWPathMonitor()
+    
+    lazy var downloadsSession: URLSession = {
+        let configuration = URLSessionConfiguration.default
+      return URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
+    }()
     
     init(videoDetail: VideoListItem) {
         videoListItem = videoDetail
+        super.init()
+        checkIfDownloadedFileExists()
+        let queue = DispatchQueue(label: "Monitor")
+        monitor.start(queue: queue)
+        checkIfInternetExists()
     }
+    
+    func startDownload(){
+        let url = URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4")
+        self.download.task = self.downloadsSession.downloadTask(with: url!)
+        self.download.task?.resume()
+    }
+    
+    func cancelDownload() {
+        download.task?.cancel()
+    }
+    
+    func checkIfDownloadedFileExists(){
+        let fileMangerApi = FileManagerApi(fileName: "video.mp4")
+        do {
+            if try fileMangerApi.checkifileExixts() {
+                self.didFinishDownloading = true
+            } else {
+                self.didFinishDownloading = false
+            }
+        } catch {
+            self.didFinishDownloading = false
+        }
+    }
+    
+    func checkIfInternetExists(){
+        monitor.pathUpdateHandler = { path in
+            if path.status == .satisfied {
+                DispatchQueue.main.async {
+                    self.isInternetConnected = true
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.isInternetConnected = false
+                }
+            }
+        }
+    }
+
+}
+
+extension VideoDetailViewModel: URLSessionDownloadDelegate {
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        let fileMangerApi = FileManagerApi(fileName: "video.mp4")
+        do {
+            let videoData = try Data(contentsOf: location)
+            do {
+                try fileMangerApi.saveFileToCache(data: videoData)
+                DispatchQueue.main.async {
+                    self.didFinishDownloading = true
+                }
+            } catch {
+                self.didFinishDownloading = false
+            }
+        }catch {
+            self.didFinishDownloading = false
+        }
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+
+        DispatchQueue.main.async {
+            let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+            let totalSize = ByteCountFormatter.string(fromByteCount: totalBytesExpectedToWrite, countStyle: .file)
+            self.percentage = progress * 100
+            self.currentProgress = String(format: "%.1f%% of %@", progress * 100, totalSize)
+        }
+    }
+    
 }
