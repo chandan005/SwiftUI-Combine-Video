915761df4e2f2437621df163ee1e64bb843385e2
chandansingh005 <chandan.singh@brauz.com>
2020-06-22 21:32:48 +0545

    Initial Commit

---
+++ SwiftUI-Combine-Video/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Combine-Video
+//
+//  Created by Chandan Singh on 6/22/20.
+//  Copyright © 2020 Developer005. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
